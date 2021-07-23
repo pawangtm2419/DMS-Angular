a5f1f6c12d14f1f4f0c2bb7dada3d06c7591d874
Praveen Kushwaha <pawangtm2419@gmail.com>
2021-07-23 11:35:30 +0530

    Date filter in invoice report

--- src/app/dealer/invoice/invoice.component.ts
+++ src/app/dealer/invoice/invoice.component.ts
@@ -12,17 +12,37 @@ export class InvoiceComponent implements OnInit {
   pageData: number = 1;
   limits = [{ "key": "50", "value": 50 }, { "key": "100", "value": 100 }, { "key": "250", "value": 250 }, { "key": "500", "value": 500 }];
   limit: any = 50;
+  fromDate: any;
+  toDate: any;
+  currentDate: any;
 
-  constructor(private dealer: DealerService, public toaster: ToasterService) { }
+  constructor(private dealer: DealerService, public toaster: ToasterService) {
+    this.fromDate =  new Date(new Date().getFullYear(), new Date().getMonth(), 1, 0, 0, 0, 0).toISOString();
+    this.toDate =  new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate(), 0, 0, 0, 0).toISOString();
+   }
 
   ngOnInit(): void {
     this.getinvoiceData();
+    this.convertDate();
   }
 
+  convertDate() {
+    function pad(s: string | number) { return (s < 10) ? '0' + s : s; }
+    var d = new Date()
+    this.currentDate = [d.getFullYear(), pad(d.getMonth()+1), pad(d.getDate())].join('-');
+    console.log(this.currentDate);
+  }
   getinvoiceData() {
-    let data = {"type":"ALL","fromDate":"2021-01-01T00:00:00.000Z","toDate":"2021-07-22T00:00:00.000Z","page":"report","useType":"ALL"};
+    let data = {
+      "type":"ALL",
+      "fromDate":(<HTMLInputElement>document.getElementById("fromDate")).value+"T00:00:00.000Z",
+      "toDate":(<HTMLInputElement>document.getElementById("toDate")).value+"T00:00:00.000Z",
+      "page":"report",
+      "useType":"ALL"
+    };
     this.dealer.invoiceReport(data).subscribe(res=> {
       this.invoiceData=res.data;
+      this.limits.splice(4);
       this.limits.push({ "key": "ALL", value: this.invoiceData.length });
       if(this.invoiceData.length > 0) {
         this.toaster.showSuccess("Data", "Report successfully Open.");
@@ -33,7 +53,9 @@ export class InvoiceComponent implements OnInit {
       this.toaster.showError("Error", error);
     });
   }
+
   dataLimit() {
     this.limit = (<HTMLInputElement>document.getElementById("limit")).value;
   }
+
 }
