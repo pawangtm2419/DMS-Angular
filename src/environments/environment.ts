37f7757935bf2501d03c786acb09e807139f7e4b
Praveen Kushwaha <pawangtm2419@gmail.com>
2021-07-23 14:20:15 +0530

    update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,10 +4,9 @@
 
 export const environment = {
   production: false,
-  /* url: "http://localhost:3041", */
+  url: "http://172.16.15.18:3041"
   /* url: "http://172.16.15.30:4000/" */
   /* url: "http://103.149.113.99:3041" */
-  url: "http://172.16.15.18:3041"
 };
 
 /*
