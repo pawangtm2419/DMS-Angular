e9b4b635c743317d113fb3750b2fd6d6ae4130ca
Praveen Kushwaha <pawangtm2419@gmail.com>
2021-07-28 17:30:06 +0530

    date filter in depot invoice report

--- src/app/depot/depot-invoice/depot-invoice.component.ts
+++ src/app/depot/depot-invoice/depot-invoice.component.ts
@@ -12,14 +12,34 @@ export class DepotInvoiceComponent implements OnInit {
   pageData = 1;
   limits: any;
   limit: any = 50;
-  constructor(private dealer: DepotService, public toaster: ToasterService) { }
+  currentDate: any;
+  fromDate: any;
+  toDate: any;
+
+  constructor(private depot: DepotService, public toaster: ToasterService) {
+    this.fromDate =  new Date(new Date().getFullYear(), new Date().getMonth(), 1, 0, 0, 0, 0).toISOString();
+    this.toDate =  new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate(), 0, 0, 0, 0).toISOString();
+   }
 
   ngOnInit(): void {
     this.getdepotInvoiceList();
+    this.convertDate();
   }
+
+  convertDate() {
+    function pad(s: string | number) { return (s < 10) ? '0' + s : s; }
+    const d = new Date();
+    this.currentDate = [d.getFullYear(), pad(d.getMonth() + 1), pad(d.getDate())].join('-');
+  }
+
   getdepotInvoiceList() {
-    const data = {type: 'ALL', fromDate: '2021-01-01T00:00:00.000Z', toDate: '2021-07-27T00:00:00.000Z', useType: 'ALL'};
-    this.dealer.depotInvoices(data).subscribe(res => {
+    const data = {
+      type: 'ALL',
+      fromDate: (document.getElementById('fromDate') as HTMLInputElement).value + 'T00:00:00.000Z',
+      toDate: (document.getElementById('toDate') as HTMLInputElement).value + 'T00:00:00.000Z',
+      useType: 'ALL'
+    };
+    this.depot.depotInvoices(data).subscribe(res => {
       this.invoiceData = res.data;
       if (this.invoiceData.length > 0) {
         this.limits = [{ key: 50, value: 50 }, { key: 100, value: 100 }, { key: 250, value: 250 }, { key: 500, value: 500 }, { key: 'ALL', value: this.invoiceData.length }];
