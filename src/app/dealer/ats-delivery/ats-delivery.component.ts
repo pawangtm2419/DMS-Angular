985a9b51f9532b20cd08d0f484a0d9f11a4b856d
Pawan Gautam <pawangtm2419@gmail.com>
2021-08-13 18:33:38 +0530

    Filter with form in ATS Delivery

--- src/app/dealer/ats-delivery/ats-delivery.component.ts
+++ src/app/dealer/ats-delivery/ats-delivery.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, NgForm } from '@angular/forms';
 import { CommonService, ToasterService } from 'src/app/shared/services';
 import * as XLSX from 'xlsx';
+
+interface selectedObject {
+  [key: string]: any
+}
 
 @Component({
   selector: 'app-ats-delivery',
@@ -9,18 +14,79 @@ import * as XLSX from 'xlsx';
 })
 export class AtsDeliveryComponent implements OnInit {
   searchData: any;
-  atsDelData: any;
+  atsDelData: string[] = [];
   pageData = 1;
   limits: any;
   limit: any = 50;
-  data: any = {"type":"PHYSICAL","fromDate":"2020-06-02T00:00:00.000Z","toDate":"2021-08-12T00:00:00.000Z","code":"10003253","state":"Gujarat","zone":"ZONE2","useType":"ALL"}
-  constructor(private service: CommonService, public toaster: ToasterService) { }
+  zoneList: any;
+  selectedZoneName: any = '';
+  selectedStateName: any = '';
+  data : selectedObject = { "type":"PHYSICAL", "useType":"ALL" };
+  stateListData: any;
+  dealerListData: any;
+  fromDate: string;
+  toDate: string;
+  currentDate: any;
+  constructor(private service: CommonService, public toaster: ToasterService) { 
+    this.fromDate =  new Date(new Date().getFullYear(), new Date().getMonth(), 1, 0, 0, 0, 0).toISOString();
+    this.toDate =  new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate(), 0, 0, 0, 0).toISOString();
+  }
 
   ngOnInit(): void {
-    this.getAtsDeliveryList()
+    this.getZoneList();
+    this.convertDate();
   }
-  getAtsDeliveryList() {
-    this.service.getVehicleDetails(this.data).subscribe(res => {
+
+  convertDate(): void{
+    function pad(s: string | number) { return (s < 10) ? '0' + s : s; }
+    const d = new Date();
+    this.currentDate = [d.getFullYear(), pad(d.getMonth() + 1), pad(d.getDate())].join('-');
+  }
+  getZoneList(): void {
+    this.service.getZones().subscribe(data => {
+      if(data.status === 'true') {
+        this.zoneList = data.zones;
+      }
+    },
+      error => console.log(error)
+    );
+  }
+  getStateList(data: any): void {
+    this.service.getStatesByZone(data).subscribe(data => {
+      if(data.status === 'true') {
+        this.stateListData = data.states;
+      }
+    },
+      error => console.log(error)
+    );
+  }
+  getDealers(data: any): void {
+    this.service.getDealerList(data).subscribe(data => {
+      if(data.status === 'true') {
+        this.dealerListData = data.msg;
+      }
+    },
+      error => console.log(error)
+    );
+  }
+  getAtsDeliveryList(atsFilterForm: NgForm) {
+    const atsFilter = atsFilterForm.value;
+    if(atsFilter.zoneCode) {
+      this.data['zone'] = atsFilter.zoneCode;
+    }
+    if(atsFilter.stateName) {
+      this.data['state'] = atsFilter.stateName;
+    }
+    if(atsFilter.dealerCode) {
+      this.data['code'] = atsFilter.dealerCode;
+    }
+    if(atsFilter.fromDate) {
+      this.data['fromDate'] = atsFilter.fromDate+"T00:00:00.000Z";
+    }
+    if(atsFilter.toDate) {
+      this.data['toDate'] = atsFilter.toDate+"T00:00:00.000Z";
+    }
+    this.service.getVehicleDetails(this.data).subscribe((res: any) => {
       this.atsDelData = res.data;
       if (this.atsDelData.length > 0) {
         this.limits = [{ key: 50, value: 50 }, { key: 100, value: 100 }, { key: 250, value: 250 }, { key: 500, value: 500 }, { key: 'ALL', value: this.atsDelData.length }];
